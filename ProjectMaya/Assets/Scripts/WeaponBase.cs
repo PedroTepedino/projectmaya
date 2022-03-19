80fd77d7ac03b37d47313f3f8cabcaad02d41f25
Rafael <rusemy.10@gmail.com>
2022-03-19 17:53:23 -0300

    Weapon Base
    Need to discuss Recharge() waiting time implementation.

---
+++ ProjectMaya/Assets/Scripts/WeaponBase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WeaponBase : MonoBehaviour
+{
+    public int projectileDamage;
+    public int magazineSize;
+    public int magazineRemaning;
+    public float reloadTime;
+    public float shootingSpeed;
+
+    public abstract void Shoot();
+
+    public virtual void Reload()
+    {
+        //wait reloadTime
+        magazineRemaning = magazineSize;
+    }
+
+}
