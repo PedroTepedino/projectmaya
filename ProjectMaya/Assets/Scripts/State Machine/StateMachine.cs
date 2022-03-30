629c7dc70ed5e4cc4fa797095186c9df7fb030df
Rafael <rusemy.10@gmail.com>
2022-03-30 18:25:49 -0300

    State Machine
    State machine codes added to project.

---
+++ ProjectMaya/Assets/Scripts/State Machine/StateMachine.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    private readonly List<StateTransition> _anyStateTransitions = new List<StateTransition>();
+
+    private readonly Dictionary<IState, List<StateTransition>> _stateTransitions =
+        new Dictionary<IState, List<StateTransition>>();
+
+    public IState LastState { get; private set; }
+
+    public IState CurrentState { get; private set; }
+
+    public event Action<IState> OnStateChanged;
+
+    public void AddAnyTransition(IState to, Func<bool> condition)
+    {
+        StateTransition stateTransition = new StateTransition(null, to, condition);
+        _anyStateTransitions.Add(stateTransition);
+    }
+
+    public void AddTransition(IState from, IState to, Func<bool> condition)
+    {
+        if (_stateTransitions.ContainsKey(from) == false)
+            _stateTransitions[from] = new List<StateTransition>();
+
+        StateTransition stateTransition = new StateTransition(from, to, condition);
+        _stateTransitions[from].Add(stateTransition);
+    }
+
+    public void SetState(IState state)
+    {
+        if (CurrentState == state)
+            return;
+
+        CurrentState?.OnExit();
+
+        LastState = CurrentState;
+        CurrentState = state;
+        CurrentState.OnEnter();
+
+        OnStateChanged?.Invoke(CurrentState);
+    }
+
+    public void Tick()
+    {
+        StateTransition transition = CheckForTransition();
+
+        if (transition != null)SetState(transition.To);
+
+        CurrentState.Tick();
+    }
+
+    private StateTransition CheckForTransition()
+    {
+        foreach (StateTransition transition in _anyStateTransitions)
+            if (transition.Condition())
+                return transition;
+        if (_stateTransitions.ContainsKey(CurrentState))
+            foreach (StateTransition transition in _stateTransitions[CurrentState])
+                if (transition.From == CurrentState && transition.Condition())
+                    return transition;
+         return null;
+    }
+}
---
+++ ProjectMaya/Assets/Scripts/State Machine/StateTransition.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class StateTransition
+{
+    public readonly Func<bool> Condition;
+    public readonly IState From;
+    public readonly IState To;
+
+    public StateTransition(IState from, IState to, Func<bool> condition)
+    {
+        From = from;
+        To = to;
+        Condition = condition;
+    }
+}
