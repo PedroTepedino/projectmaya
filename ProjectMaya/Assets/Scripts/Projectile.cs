2f7e7cac60d0dc5fc181d829ebd816de78ef560b
Rafael <rusemy.10@gmail.com>
2022-04-28 20:06:27 -0300

    Projectile changes
    Added Move and "Destroy" functions

--- ProjectMaya/Assets/Scripts/Projectile.cs
+++ ProjectMaya/Assets/Scripts/Projectile.cs
@@ -6,7 +6,33 @@ public abstract class Projectile : MonoBehaviour
 {
     public int damage;
     public float speed;
+    public float lifetime = 5f;
     public LayerMask layersToHit;
+    public Vector2 direction { get;  set; }
+    
+    protected void OnEnable() {
+        Invoke("Destroy", lifetime);
+    }
+
+    protected void FixedUpdate() {
+        Move();
+    }
+
+    public virtual void Move()
+    {
+        transform.Translate(direction * speed, Space.World);
+    }
+
+    protected void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    protected private void OnDisable() {
+        CancelInvoke();
+    }
 
     public abstract IEnumerator Modifier();
+
+
 }
