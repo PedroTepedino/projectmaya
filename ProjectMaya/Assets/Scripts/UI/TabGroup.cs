ca959c89310627a4db1ec2df1b59f50f68012b7a
Rafael <rusemy.10@gmail.com>
2022-05-26 18:23:19 -0300

    Menus implementation started
    Started implementing UI scripts, added TMPro essentials and prototype sprites for menu

---
+++ ProjectMaya/Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public TabButton selectedTab;
+    public Sprite tabIdleSprite;
+    public Sprite tabHoverSprite;
+    public Sprite tabActiveSprite;
+    public List<GameObject> objectsToSwap;
+
+    public void Subscribe(TabButton button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.background.sprite = tabHoverSprite;
+        }
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        if (selectedTab != null)
+        {
+            selectedTab.Deselected();
+        }
+        selectedTab = button;
+        selectedTab.Selected();
+        ResetTabs();
+        button.background.sprite = tabActiveSprite;
+        int index = button.transform.GetSiblingIndex();
+        for (int i = 0; i < objectsToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }else
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach (TabButton button in tabButtons)
+        {
+            if (selectedTab != null && button == selectedTab ) 
+            {
+                continue;
+            }
+            
+            button.background.sprite = tabIdleSprite;
+        }
+    }
+}
