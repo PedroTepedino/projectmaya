077cd1579884fb30ab66d2a4cd5ec12bae27893d
illuceo <90065614+illuceo@users.noreply.github.com>
2022-06-27 22:10:18 -0300

    Revert "Modificações no prefab do player (implementação de alguns SFX) + adição dos prefabs das músicas de fundo"
    This reverts commit 2ae07884590afde10b78c841cc3b7aae174243d7.

--- ProjectMaya/Assets/Scripts/Player/PlayerAudioController.cs
+++ ProjectMaya/Assets/Scripts/Player/PlayerAudioController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerAudioController : MonoBehaviour
 { 
+    public FMODUnity.EventReference PlayerDashSound;
+    public FMODUnity.EventReference PlayerHitSound;
     public FMODUnity.EventReference PlayerIdleSoundEvent;
     public FMODUnity.EventReference PlayerMoveSoundEvent;
 
@@ -20,18 +22,28 @@ public class PlayerAudioController : MonoBehaviour
 
     void Update()
     {
-        if (rb.velocity.magnitude > 0.5 && !IsPlaying(PlayerMoveSound))
+        if (rb.velocity.magnitude > 0 && !IsPlaying(PlayerMoveSound))
         {
             PlayerMoveSound.start();
             PlayerIdleSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         }
 
-        if (rb.velocity.magnitude <= 0.5 && IsPlaying(PlayerMoveSound))
+        if (rb.velocity.magnitude == 0 && IsPlaying(PlayerMoveSound))
         {
             //PlayerMoveSound.release();
             PlayerIdleSound.start();
             PlayerMoveSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         }
+    }
+
+    void PlayDashSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(PlayerDashSound, transform.position);
+    }
+
+    void PlayHitSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(PlayerHitSound, transform.position);
     }
 
     bool IsPlaying(FMOD.Studio.EventInstance instance)
