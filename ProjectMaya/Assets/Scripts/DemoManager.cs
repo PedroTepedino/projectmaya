44be7d36c19b5680e13ff7a1c82148080118e19b
Rafael <rusemy.10@gmail.com>
2022-10-10 20:05:39 -0300

    DemoManager created to Add win screen

---
+++ ProjectMaya/Assets/Scripts/DemoManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DemoManager : MonoBehaviour
+{
+    private static DemoManager instance = null;
+    public static DemoManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<DemoManager>();
+                if (instance == null)
+                {
+                    GameObject managerDemoObject = new GameObject("DemoManager", typeof(DemoManager));
+                    instance = managerDemoObject.GetComponent<DemoManager>();
+                }
+            }
+            return instance;
+        }
+
+    }
+
+
+    [SerializeField] private GameObject winScreen;
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private LifeSystem bossLifeSystem;
+    [SerializeField] private SceneTransitionManager sceneManager;
+    [SerializeField] private string menuSceneName;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance == this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        bossLifeSystem.OnDie += ShowWinScreen;
+    }
+
+    private void OnDisable()
+    {
+        bossLifeSystem.OnDie -= ShowWinScreen;
+    }
+
+    public void GoToMainMenu()
+    {
+        sceneManager.SwitchToScene(menuSceneName);
+    }
+
+    public void ShowWinScreen()
+    {
+        winScreen.SetActive(true);
+        Time.timeScale = 0f;
+        playerInput.SwitchCurrentActionMap("UI");
+    }
+}
