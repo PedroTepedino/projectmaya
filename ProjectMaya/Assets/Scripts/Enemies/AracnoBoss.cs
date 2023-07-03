78fd2d3d1e9fc828dba63fa5882e40817b0615fe
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2023-07-03 15:24:50 -0300

    Test Aracno Boss
    Modos 1 e 2 do boss COMENTADOS. Boss indo direto para o modo 3 de combate.

--- ProjectMaya/Assets/Scripts/Enemies/AracnoBoss.cs
+++ ProjectMaya/Assets/Scripts/Enemies/AracnoBoss.cs
@@ -12,17 +12,17 @@ public class AracnoBoss : EnemyStateMachine
         lifeSystem = GetComponent<LifeSystem>();
 
         IdleState idleState = new IdleState(gameObject);
-        Phase1AracnoState phase1AracnoState = new Phase1AracnoState(gameObject);
-        Phase2AracnoState phase2AracnoState = new Phase2AracnoState(gameObject);
+        //Phase1AracnoState phase1AracnoState = new Phase1AracnoState(gameObject);
+        //Phase2AracnoState phase2AracnoState = new Phase2AracnoState(gameObject);
         Phase3AracnoState phase3AracnoState = new Phase3AracnoState(gameObject);
 
         stateMachine.SetState(idleState);
 
-        stateMachine.AddTransition(idleState, phase1AracnoState, () => targeting.hasTarget);
-        stateMachine.AddTransition(phase1AracnoState, phase2AracnoState, () => targeting.hasTarget && (lifeSystem._currentLife < lifeSystem.MaxLife/2));
-        stateMachine.AddTransition(phase2AracnoState, phase3AracnoState, () => targeting.hasTarget && (lifeSystem._currentLife < lifeSystem.MaxLife/4));
-        stateMachine.AddTransition(phase1AracnoState, idleState, () => !targeting.hasTarget);
-        stateMachine.AddTransition(phase2AracnoState, idleState, () => !targeting.hasTarget);
+        stateMachine.AddTransition(idleState, phase3AracnoState, () => targeting.hasTarget);
+        //stateMachine.AddTransition(phase1AracnoState, phase2AracnoState, () => targeting.hasTarget && (lifeSystem._currentLife < lifeSystem.MaxLife/2));
+        //stateMachine.AddTransition(phase2AracnoState, phase3AracnoState, () => targeting.hasTarget && (lifeSystem._currentLife < lifeSystem.MaxLife/4));
+        //stateMachine.AddTransition(phase1AracnoState, idleState, () => !targeting.hasTarget);
+        //stateMachine.AddTransition(phase2AracnoState, idleState, () => !targeting.hasTarget);
         stateMachine.AddTransition(phase3AracnoState, idleState, () => !targeting.hasTarget);
     }
 }
